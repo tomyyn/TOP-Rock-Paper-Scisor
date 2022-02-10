5b2605fc9d5eaa4f0ce7f6ff4bcd23165387e555
Tom <tomasnaiouf@gmail.com>
2022-02-10 16:44:24 -0300

    Add game engine and player input

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Choose computer selection
 function computerPlay()
 {
     let randN = Math.floor(Math.random()*3);
@@ -8,18 +9,54 @@ function computerPlay()
     return retVal 
 }
 
+//Verify if an input is valid
 function checkInput(str){
     return(str=="rock"||str=="paper"||str=="scisor")
 }
 
+//Read user input until it is correct
+function getInput(){
+    let input = window.prompt("Enter your selection!")
+    while(!checkInput(input)){
+        console.log("Invalid selection");
+        input = window.prompt("Enter your selection!")
+    }
+    return input;
+
+}
+
+//Play one round
 function playRound(playerSelection,computerSelection)
 {
+    let retVal;
+
     playerSelection=playerSelection.toLowerCase();
     computerSelection=computerSelection.toLowerCase();
-    let retVal;
-    if(playerSelection==computerSelection) retVal=`Draw! you both choosed ${playerSelection}`; 
-    else if((playerSelection=="rock" && computerSelection=="scisor")||(playerSelection=="scisor" && computerSelection=="paper")||(playerSelection=="paper" && computerSelection=="rock")) retVal=`You won! ${playerSelection} beats ${computerSelection}`;
-    else retVal=`You lost! ${computerSelection} beats ${playerSelection}`;
-
+    console.log(computerSelection)
+    if(playerSelection==computerSelection) retVal="Draw"; 
+    else if((playerSelection=="rock" && computerSelection=="scisor")||(playerSelection=="scisor" && computerSelection=="paper")||(playerSelection=="paper" && computerSelection=="rock")) retVal="Win";
+    else retVal="Loose";
+    console.log(retVal)
     return retVal;
+}
+
+//Main game loop
+function game(){
+    let playerV=0, computerV=0, aux, playerSelection,computerSelection;
+    for(let i=0;i<5;i++){     
+        playerSelection=getInput();
+        computerSelection=computerPlay();
+        
+        aux=playRound(playerSelection,computerSelection);
+
+        if(aux=="Draw") aux=`Draw! you both choosed ${playerSelection}`; 
+        else if (aux=="Win") {
+            aux=`You won! ${playerSelection} beats ${computerSelection}`;
+        }
+        else {
+            aux=`You lost! ${computerSelection} beats ${playerSelection}`;
+        }
+        
+        console.log(aux);
+    }
 }
