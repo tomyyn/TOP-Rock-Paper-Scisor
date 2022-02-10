6eec738f8260503a9f8703aff0cd2a6f7a1b0ac7
Tom <tomasnaiouf@gmail.com>
2022-02-10 16:20:08 -0300

    Add playRound

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-const ROCK = 1;
-const PAPER = 2;
-const SCISOR = 3;
-
 function computerPlay()
 {
     let randN = Math.floor(Math.random()*3);
     let retVal;
-    if (randN==0) retVal=ROCK;
-    else if (randN==1) retVal=PAPER;
-    else retVal=SCISOR;
+    if (randN==0) retVal="ROCK";
+    else if (randN==1) retVal="PAPER";
+    else retVal="SCISOR";
     return retVal 
+}
+
+function checkInput(str){
+    return(str=="rock"||str=="paper"||str=="scisor")
+}
+
+function playRound(playerSelection,computerSelection)
+{
+    playerSelection=playerSelection.toLowerCase();
+    computerSelection=computerSelection.toLowerCase();
+    let retVal;
+    if(playerSelection==computerSelection) retVal=`Draw! you both choosed ${playerSelection}`; 
+    else if((playerSelection=="rock" && computerSelection=="scisor")||(playerSelection=="scisor" && computerSelection=="paper")||(playerSelection=="paper" && computerSelection=="rock")) retVal=`You won! ${playerSelection} beats ${computerSelection}`;
+    else retVal=`You lost! ${computerSelection} beats ${playerSelection}`;
+
+    return retVal;
 }
